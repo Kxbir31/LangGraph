0af1f3c8d311a6a95a3529b3921007c189881d37
Kabir Bhardwaj <kabirbhardwaj026@gmail.com>
2026-08-21 04:22:14 +0530

    z

--- React_Agent.py
+++ React_Agent.py
@@ -1,15 +1,24 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
-from langchain_community.tools import TavilySearchResults
+from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
 from numpy.f2py.crackfortran import verbose
 
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model = "gemini-3-flash-preview")
 
-search_tool = TavilySearchResults(search_depth = "basic")
+search_tool = TavilySearch(search_depth = "basic")
 
 tools = [search_tool]
 
-agent = create_agent(tools=tools , llm = llm, agent = "Zero - Shot react description", verbose = True)
+agent = create_agent(
+    model=llm,
+    tools=tools
+    )
 
+result = agent.invoke({
+    "messages": [
+        ("user", "Give me a funny tweet about what is the weather in Sehore")
+    ]
+})
+print(result.content)
